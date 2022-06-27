32cb2a7bd2897293be28bb81be95ad453cf796e7
Alex Luque <alexluque29@gmail.com>
2022-06-28 00:20:59 +0200

    Ejercicio 3

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 
 const CALLINGJOKES = "https://icanhazdadjoke.com/";
+let reportJokes = [];
 let printJoke;
+let date = new Date();
 
+// Llamada a la API
 let loadApi = async() =>{
     try{
 let response = await fetch(CALLINGJOKES, {
@@ -17,4 +20,22 @@ document.getElementById("writeJoke").innerHTML = printJoke;
 } catch(error){
     console.log(error);
 }
+}
+
+// Creación de la clase Register
+class Register {
+    constructor(joke, score, date) {
+        this.joke = joke;
+        this.score = score;
+        this.date = date;
+    }
+}
+// Obtención de la puntuación del chiste y creación de la instancia que se añade al array.
+function rate(id){
+    let joke = printJoke;
+    let score = id;
+    let dateIso = date.toISOString();
+    let newRegister = new Register (joke, score, dateIso);
+    reportJokes.push(newRegister);
+    console.table(reportJokes)
 }
