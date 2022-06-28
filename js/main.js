d2e34be0be2e8869b825b065638efb1c4f544d9b
Alex Luque <alexluque29@gmail.com>
2022-06-28 10:50:15 +0200

    Ejercicio 4

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 
 const CALLINGJOKES = "https://icanhazdadjoke.com/";
+const CALLINGWEATHER = "http://dataservice.accuweather.com/forecasts/v1/daily/1day/307297?apikey=jDRJs43fxDcQlMD5etovQZTl7GXiJozS";
 let reportJokes = [];
-let printJoke;
+let printJoke, printWeather,printTemp;
 let date = new Date();
 
-// Llamada a la API
+// Llamada a la API Chistes
 let loadApi = async() =>{
     try{
 let response = await fetch(CALLINGJOKES, {
@@ -38,4 +39,23 @@ function rate(id){
     let newRegister = new Register (joke, score, dateIso);
     reportJokes.push(newRegister);
     console.table(reportJokes)
+}
+
+// Llamada a la API Meteo
+let loadApiWeather = async() =>{
+    try{
+let response = await fetch(CALLINGWEATHER, {
+    headers: {
+        'Accept': "application/json"
+    }
+    });
+const data = await response.json();
+let tempF = data.DailyForecasts[0].Temperature.Maximum.Value;
+printWeather = data.DailyForecasts[0].Day.IconPhrase;
+printTemp = ((tempF - 32) * 5/9).toFixed(1);
+document.getElementById("meteo").innerHTML = `Barcelona, today's weather: <b>${printWeather}</b> Temp màx: <b>${printTemp} ºC.</b>`;
+ 
+} catch(error){
+    console.log(error);
+}
 }
